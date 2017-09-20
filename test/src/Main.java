c822e0bd693a07a4d74d1e58f839e87f1ac205e8
bjdyh0505 <819152324@qq.com>
2017-09-20 15:21:28 +0800

    在master分支上提交“hello! master branch 2017-09-20 15:20”字符串

--- test/src/Main.java
+++ test/src/Main.java
@@ -19,5 +19,6 @@ public class Main {
 
         System.out.println("hello! master branch 2017-09-20 14:56");
         System.out.println("hello! master branch 2017-09-20 14:58");
+        System.out.println("hello! master branch 2017-09-20 15:20");
     }
 }
