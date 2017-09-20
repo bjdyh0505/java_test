4ef220d9453bc553ebfd0b5edd745947929c987a
bjdyh0505 <819152324@qq.com>
2017-09-20 09:58:38 +0800

    在“develop”类里添加job属性

--- test/src/Develop.java
+++ test/src/Develop.java
@@ -5,6 +5,7 @@ public class Develop {
 
     private String name;
     private String age;
+    private String job;
 
     public String getName() {
         return name;
@@ -13,4 +14,12 @@ public class Develop {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getAge() { return age; }
+
+    public void setAge(String age) { this.age = age; }
+
+    public String getJob() { return job; }
+
+    public void setJob(String job) { this.job = job; }
 }
