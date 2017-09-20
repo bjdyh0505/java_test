bf08c7c2b5bf6edaa82c247cc338eb6b852d11a5
bjdyh0505 <819152324@qq.com>
2017-09-20 09:35:33 +0800

    在develop分支上提交“develop类”

---
+++ test/src/Develop.java
@@ -0,0 +1,16 @@
+/**
+ * Created by yuanhai.dong on 2017/9/19.
+ */
+public class Develop {
+
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
